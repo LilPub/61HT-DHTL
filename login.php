cee01b9f05592d66540a576810be0aea931c0798
LilPub <hiep140701@gmail.com>
2021-09-24 20:36:08 +0700

    add login

--- login.php
+++ login.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    <link rel="stylesheet" href="./login.css">
+    <link rel="stylesheet" href="./css/login.css">
 </head>
 <body class="text-center">
     <main class="form-signin">
       <form>
-        <img class="mb-4" src="Logo-Thuy_Loi.png" alt="" width="72" height="57">
+        <img class="mb-4" src="./img/Logo-Thuy_Loi.png" alt="" width="72" height="57">
     
         <div class="form-floating">
           <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
